316763f63c72ace77b4815b926b8cb7ae5fbfe48
Jamiesona <kinard-k@qq.com>
2020-01-17 15:56:13 +0800

    练习使用Transformer.

--- 02-Translation/translation_v2.py
+++ 02-Translation/translation_v2.py
@@ -1,30 +1,70 @@
 import torch
 import torch.nn as nn
+import re
+import torch.optim as optim
 
-# 常量定义
-HIDDEN_SIZE = 100
+SOS_TOKEN = "<SOS>"
+EOS_TOKEN = "<EOS>"
 
 
-class Encoder(nn.Module):
-    """
-    编码器.
-    """
+class Lang:
+    """表示一种语言."""
 
-    def __init__(self, input_size, hidden_size=HIDDEN_SIZE):
-        """
-        构造函数.
-        :param input_size: 输入包含的单词数量.
-        :param hidden_size: 隐含状态大小.
-        """
-        super(Encoder, self).__init__()
-        # 运行使用的设备
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.hidden_size = hidden_size
-        self.embedding = nn.Embedding(input_size, hidden_size)
-        self.gru_cell = nn.GRUCell(hidden_size, hidden_size)
+    def __init__(self):
+        self.word2idx = {SOS_TOKEN: 0, EOS_TOKEN: 1}
+        self.idx2word = {0: SOS_TOKEN, 1: EOS_TOKEN}
+        self.count = 2
 
-    def forward(self, x, hidden):
-        pass
+    def add_sentence(self, sentence):
+        raise NotImplementedError("未实现的方法.")
 
-    def init_hidden(self):
-        pass
+    def to_tensor(self, sentence):
+        raise NotImplementedError("未实现的方法.")
+
+
+class English(Lang):
+    def __init__(self):
+        super(English, self).__init__()
+
+    def add_sentence(self, sentence):
+        words = re.split(r"\s", sentence)
+        for word in words:
+            if word not in self.word2idx:
+                self.word2idx[word] = self.count
+                self.idx2word[self.count] = word
+                self.count += 1
+
+    def to_tensor(self, sentence):
+        words = re.split(r"\s", sentence)
+        lst = [self.word2idx[SOS_TOKEN]] + [self.word2idx[word]
+                                            for word in words] + [self.word2idx[EOS_TOKEN]]
+        return torch.tensor(lst)
+
+
+class Chinese(Lang):
+    def __init__(self):
+        super(Chinese, self).__init__()
+
+    def add_sentence(self, sentence):
+        for word in sentence:
+            if word not in self.word2idx:
+                self.word2idx[word] = self.count
+                self.idx2word[self.count] = word
+                self.count += 1
+
+    def to_tensor(self, sentence):
+        lst = [self.word2idx[SOS_TOKEN]] + [self.word2idx[word]
+                                            for word in sentence] + [self.word2idx[EOS_TOKEN]]
+        return torch.tensor(lst)
+
+
+class Translator(nn.Module):
+    """使用transformer实现翻译器."""
+
+    def __init__(self, input_lang, target_lang, hidden_size):
+        self.input_lang = input_lang
+        self.target_lang = target_lang
+        self.input_embedding = nn.Embedding(input_lang.count, hidden_size)
+        self.target_embedding = nn.Embedding(target_lang.count, hidden_size)
+        self.trsfm = nn.Transformer(hidden_size, 3, 3)
+        self.loss = nn.MSELoss()
